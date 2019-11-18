16998709be9c189776f2174539105ca678e71e0e
n.antiya <n.antiya@dss-509.dsslp.com>
2019-11-18 11:33:23 +0530

    ok

--- integrationdemo/src/main/java/com/springtuts/integrationdemo/IntegrationdemoApplication.java
+++ integrationdemo/src/main/java/com/springtuts/integrationdemo/IntegrationdemoApplication.java
@@ -1,4 +1,4 @@
-package com.pspringtuts.integrationdemo;
+package com.springtuts.integrationdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
