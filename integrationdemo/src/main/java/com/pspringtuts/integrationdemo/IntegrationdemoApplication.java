4f004db5029d98d7f8ae080a2698c595c3113cd8
n.antiya <n.antiya@dss-509.dsslp.com>
2019-11-18 11:15:50 +0530

    new project

---
+++ integrationdemo/src/main/java/com/pspringtuts/integrationdemo/IntegrationdemoApplication.java
@@ -0,0 +1,13 @@
+package com.pspringtuts.integrationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationdemoApplication.class, args);
+	}
+
+}
